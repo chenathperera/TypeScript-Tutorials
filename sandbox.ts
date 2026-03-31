32aa606496c18f0692bb055b85001c5e786a2862
Chenath Perera <chenathperera@gmail.com>
2026-03-31 22:39:03 +0530

    tsc file compilation
    tsc filename.ts

--- sandbox.ts
+++ sandbox.ts
@@ -1,11 +1,11 @@
-<<<<<<< Updated upstream
-=======
-"use strict";
 const character = 'mario';
+
 console.log(character);
+
 const inputs = document.querySelectorAll('input');
+
 console.log(inputs);
-inputs.forEach(function (input) {
+
+inputs.forEach(function (input){
     console.log(input);
-});
->>>>>>> Stashed changes
+})
