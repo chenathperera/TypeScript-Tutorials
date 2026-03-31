b9eb523eefb45ddb188f8d7b32aac5e9a3685779
Chenath Perera <chenathperera@gmail.com>
2026-03-31 22:40:48 +0530

    tsc file compilation
    tsc filename.ts

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+"use strict";
+const character = 'mario';
+console.log(character);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
